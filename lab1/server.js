9e1802d7fce417556f127d64690830b931ebd0a4
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-06-02 09:57:58 +0700

    setup success

--- lab1/server.js
+++ lab1/server.js
@@ -3,15 +3,29 @@ const fileupload = require("express-fileupload");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path")
+
 
 app.use(cors());
 app.use(fileupload());
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
+app.use('/static' , express.static(path.join(__dirname,'public')))
 
 
-
-
+app.post("/upload", (req, res) => {
+    const newpath = __dirname + "/public/upload-files/";
+    const file = req.files.file;
+    const filename = file.name;
+   
+    file.mv(`${newpath}${filename}`, (err) => {
+      if (err) {
+        res.status(500).send({ message: "File upload failed", code: 200 });
+      }
+      res.status(200).send({ message: "File Uploaded", code: 200 });
+    });
+  });
+  
 
 
 
